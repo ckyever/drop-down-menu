d8eba4dec8533d10c8c3e1173665e5e9d306d687
ckyever <ckyeverg@gmail.com>
2025-10-06 17:44:13 +1100

    Add top and bottom button to test dropdown behaviours

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./styles.css";
+
+const containerElements = document.querySelectorAll("div.container");
+
+containerElements.forEach((container) => {
+  // Create button to trigger dropdown
+  const button = document.createElement("button");
+  button.textContent = "Click me!";
+
+  // Create array of menu options
+  const menuOptions = [];
+  const editOption = document.createElement("button");
+  editOption.textContent = "Edit";
+  menuOptions.push(editOption);
+  const renameOption = document.createElement("button");
+  renameOption.textContent = "Rename";
+  menuOptions.push(renameOption);
+  const deleteOption = document.createElement("button");
+  deleteOption.textContent = "Delete";
+  menuOptions.push(deleteOption);
+});
